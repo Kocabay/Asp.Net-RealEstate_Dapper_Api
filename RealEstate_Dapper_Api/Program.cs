2e513cf384cb8bdad338d464a405074619229f87
Ömer Kocabay <passanger54@hotmail.com>
2024-06-22 23:38:18 +0300

    Add project files.

---
+++ RealEstate_Dapper_Api/Program.cs
@@ -0,0 +1,49 @@
+using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+namespace RealEstate_Dapper_Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddTransient<Context>();
+            builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+            //builder.Services.AddTransient<IProductRepository, ProductRepository>();
+            //builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+            //builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
+            //builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
+            //builder.Services.AddTransient<IPopularLocationRepository,PopularLocationRepository>();
+            //builder.Services.AddTransient<IStatisticsRepository,StatisticsRepository>();
+            //builder.Services.AddTransient<ITestimonialRepository,TestimonialRepository>();
+            //builder.Services.AddTransient<IEmployeeRepository,EmployeeRepository>();
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

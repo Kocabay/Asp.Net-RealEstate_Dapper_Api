c257ff4c5a584e5b99c66c69c66ec6fc33a7bdc0
Ömer Kocabay <passanger54@hotmail.com>
2024-09-03 22:22:36 +0300

    signalR

---
+++ RealEstate_Dapper_Api/Hubs/SignalRHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealEstate_Dapper_Api.Hubs
+{
+    public class SignalRHub:Hub
+    {
+        
+    }
+}
--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,3 +1,4 @@
+using RealEstate_Dapper_Api.Hubs;
 using RealEstate_Dapper_Api.Models.DapperContext;
 using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
 using RealEstate_Dapper_Api.Repositories.BottomGridRepository;
@@ -30,11 +31,23 @@ namespace RealEstate_Dapper_Api
             builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
             builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
             builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
-            builder.Services.AddTransient<IStatisticRepository,StatisticRepository>();
-            builder.Services.AddTransient<IEmployeeRepository,EmployeeRepository>();
+            builder.Services.AddTransient<IStatisticRepository, StatisticRepository>();
+            builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
             builder.Services.AddTransient<IContactRepository, ContactRepository>();
-            builder.Services.AddTransient<IToDoListRepository,ToDoListRepository>();
+            builder.Services.AddTransient<IToDoListRepository, ToDoListRepository>();
 
+            builder.Services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .SetIsOriginAllowed((host) => true)
+                            .AllowCredentials();
+                });
+            });
+
+            builder.Services.AddSignalR();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -50,12 +63,16 @@ namespace RealEstate_Dapper_Api
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
 
+
             app.MapControllers();
+
+            app.MapHub<SignalRHub>("/signalrhub");
 
             app.Run();
         }

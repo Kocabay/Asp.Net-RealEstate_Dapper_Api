d47579ecf7547950bbb9df2959a948c512bb1194
Ömer Kocabay <passanger54@hotmail.com>
2024-12-20 18:22:17 +0300

    Amenity

---
+++ RealEstate_Dapper_UI/Dtos/PropertyAmenityDtos/ResultPropertAmenityByStatusTrueDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.PropertyAmenityDtos
+{
+    public class ResultPropertAmenityByStatusTrueDto
+    {
+        public int PropertyAmenityId { get; set; }
+        public string Title { get; set; }
+    }
+
+}
